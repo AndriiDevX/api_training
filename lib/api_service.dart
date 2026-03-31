5736ef4dcdac5f5888c95e0057d8de96c940278d
AndriiDevX <sapsayandrey1@gmail.com>
2026-03-31 19:28:55 +0300

    Add  Post model and JSON parsing

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -1,5 +1,6 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'model.dart';
 
 class ApiService {
   Future fetchData() async {
@@ -9,6 +10,10 @@ class ApiService {
     
     );
     final data = jsonDecode(response.body);
+    final List posts = data['posts'];
+    final List<Post> postList = posts.map((e) => Post.fromJson(e)).toList();
+
+    print(postList[0].title);
    
     print(data['posts'][0]['title']);
   }
